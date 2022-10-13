fa7f382a60d36090ef91417c8d2ba36e90aa0e24
Josh Orr <josho39@gmail.com>
2022-10-13 11:18:43 -0600

    fix: style

--- tests/test_unwrap.py
+++ tests/test_unwrap.py
@@ -6,7 +6,7 @@ from guards.unwrap import UnwrapResults
 
 @pytest.mark.parametrize("test_type,expected_result", [
     (str, UnwrapResults(str, False, False)),
-    (Nullable[int],  UnwrapResults(int, True, False)),
+    (Nullable[int], UnwrapResults(int, True, False)),
     (Optional[Nullable[str]], UnwrapResults(str, True, True)),
     (Optional[List[str]], UnwrapResults(List[str], False, True))
 ])
